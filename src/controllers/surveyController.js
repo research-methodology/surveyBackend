ba8f0a36c47f279651b3edccfa0c2dcd0d561b41
nkbtemmy <emmanuelnkubito2@gmail.com>
2021-08-04 12:10:50 +0200

    correct saveSurvey function

--- src/controllers/surveyController.js
+++ src/controllers/surveyController.js
@@ -2,76 +2,79 @@ import {Survey} from './../database/schema/survey'
 class surveyController{
 
   static async createSurvey(req,res){
-    const data = req.body.categories
-    var url="";
-    var surveyId="" ;
-    const initialData = {
-      surveyTitle: req.body.surveyTitle,
-      results:[]
-    }
-    await Survey.create(initialData)
-      .then((response) => {
-        surveyId = response._id;
-        response.userId = req.id;
+        const data = req.body.categories
+        var url="";
+        var surveyId="" ;
+        const initialData = {
+          surveyTitle: req.body.surveyTitle,
+          results:[]
+        }
+        await Survey.create(initialData)
+          .then((response) => {
+            surveyId = response._id;
+            response.userId = req.id;
+ 
+            data.forEach((category) => {
+             //insering categoryname 
+              response.categories.push({
+                categoryName: category.categoryName,
+                questions: [],
+              });
 
-        data.forEach((category) => {
-         //insering categoryname 
-          response.categories.push({
-            categoryName: category.categoryName,
-            questions: [],
-          });
-​
-          category.questions.forEach(async(question) => {
-          //each category from frontend
-          const respCatLength= response.categories.length;
-          let firstLength = respCatLength;
-             response.categories[firstLength - 1].questions.push({
-              question: question.question,
-              wayOfAnswering: question.wayOfAnswering,
-              answers: [],
-            });
-​
-        
-          question.answers.forEach((answer) => {
-              //console.log(answer);
-              let cateLeng = respCatLength;
-              let leng =response.categories[cateLeng - 1].questions.length;
-              response.categories[cateLeng-1].questions[ leng - 1 ].answers.push({
-                  answer:answer.answer
+              category.questions.forEach(async(question) => {
+              //each category from frontend
+              const respCatLength= response.categories.length;
+              let firstLength = respCatLength;
+                 response.categories[firstLength - 1].questions.push({
+                  question: question.question,
+                  wayOfAnswering: question.wayOfAnswering,
+                  answers: [],
                 });
-            });  
-      ​
-          });   
-​
-        });
 
-        response.save().then((resp)=>{
-          url = `https://cst-survey-frontend.herokuapp.com/respondent/${surveyId}`;
-          return res.status(201).json({
-            status: 201,
-            message: "survey created sucessfull !!!!!",
-            surveyURL:url,
-            response:resp
-          });
-        }).catch((error)=>{
-          res.status(403).json({
-            status:403,
-            message:'fail to save survey',
-            errorMessage:error.message
+            
+              question.answers.forEach((answer) => {
+                  //console.log(answer);
+                  let cateLeng = respCatLength;
+                  let leng =response.categories[cateLeng - 1].questions.length;
+                  response.categories[cateLeng-1].questions[ leng - 1 ].answers.push({
+                      answer:answer.answer
+                    });
+                });  
+            
+
+
+              });
+              
+
+            });
+  
+            response.save().then((resp)=>{
+              url = `https://cst-survey-frontend.herokuapp.com/respondent/${surveyId}`;
+              return res.status(201).json({
+                status: 201,
+                message: "survey created sucessfull !!!!!",
+                surveyURL:url,
+                response:resp
+              });
+            }).catch((error)=>{
+              res.status(403).json({
+                status:403,
+                message:'fail to save survey',
+                errorMessage:error.message
+              })
+            })
+            
           })
-        })
-        
-      })
-      .catch((err) => {
-        //console.log(error);
-        return res.status(500).send({
-          status: 500,
-          errorMessage: err.message,
-          message: "server Error",
-        });
-      });
-}
-​
+          .catch((err) => {
+            //console.log(error);
+            return res.status(500).send({
+              status: 500,
+              errorMessage: err.message,
+              message: "server Error",
+            });
+          });
+  }
+
 static async getOneSurvey(req, res) {
   await Survey.findById(req.params.id)
     .then((survey) => {
